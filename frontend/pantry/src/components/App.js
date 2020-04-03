e76e2f699df1c3a8286be51554566e227b517e6e
Jenesh <jenesh.napit@gmail.com>
2020-04-03 19:15:15 -0400

    Installed libraries for navigation and updated README.md

--- frontend/pantry/src/components/App.js
+++ frontend/pantry/src/components/App.js
@@ -1,25 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
   StyleSheet,
   View,
   Text,
 } from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
   return (
