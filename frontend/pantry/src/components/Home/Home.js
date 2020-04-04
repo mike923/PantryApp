4c30bda8a32512044dccd5fe3023c2f0c86a10da
Jenesh <jenesh.napit@gmail.com>
2020-04-03 22:20:50 -0400

    Added bottom tab navigation and created Screen components with stack navigation

--- frontend/pantry/src/components/Home/Home.js
+++ frontend/pantry/src/components/Home/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
-const Home = ({ text, navigation }) => {
+const Home = ({ navigation }) => {
   const navigateToUsers = () => navigation.navigate('User')
   return (
     <View style={styles.container}>
-      <Text>{text}</Text>
+      <Text>Home</Text>
       <TouchableOpacity onPress={navigateToUsers} style={styles.button}>
         <Text style={styles.buttonText}>Go to Users</Text>
       </TouchableOpacity>
