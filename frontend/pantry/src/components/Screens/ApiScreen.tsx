7d285ac8da858f591aeccac8d062c585a0720278
vonbarown <vonielbrown@pursuit.org>
2020-04-12 00:23:50 -0400

    getting a camera component to the screen

--- frontend/pantry/src/components/Screens/ApiScreen.tsx
+++ frontend/pantry/src/components/Screens/ApiScreen.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import ApiTester from '../testComp/ApiTester';
-import ImageUpload from '../testComp/ImageUpload/ImageUpload';
-
+// import ImageUpload from '../testComp/ImageUpload/ImageUpload';
+import CameraTest from '../testComp/ImageUpload/reactNativeCamera';
 const Stack = createStackNavigator();
 
 const ApiScreen = ({ navigation }) => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Api" component={ApiTester} />
-      <Stack.Screen name="ImageUpload" component={ImageUpload} />
+      <Stack.Screen name="ImageUpload" component={CameraTest} />
     </Stack.Navigator>
   );
 };
