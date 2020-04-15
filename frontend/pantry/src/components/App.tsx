e1299401ec5e3a3898149cc65f8ed9ac537936ab
vonbarown <vonielbrown@pursuit.org>
2020-04-14 22:49:50 -0400

    adding vectors to the project updating files

--- frontend/pantry/src/components/App.tsx
+++ frontend/pantry/src/components/App.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import 'react-native-gesture-handler'; // Requred for @react-navigation
 import { NavigationContainer } from '@react-navigation/native'; // Navigation wrapper for App
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { Login } from './auth/Login';
+import { Register } from './auth/Register';
+import Home from './Home/Home';
+import { Reset } from './auth/Reset';
 
 import store from '../redux/store';
 import { Provider } from 'react-redux';
@@ -12,20 +18,49 @@ import UserScreen from './Screens/UserScreen';
 import ApiScreen from './Screens/ApiScreen';
 // import TestScreen from './Screens/TestScreen';
 
-const Tab = createBottomTabNavigator();
+// const Tab = createBottomTabNavigator();
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <Provider store={store}>
+//       <NavigationContainer>
+//         <Tab.Navigator>
+//           <Tab.Screen name="Home" component={HomeScreen} />
+//           <Tab.Screen name="User" component={UserScreen} />
+//           <Tab.Screen name="API" component={ApiScreen} />
+//         </Tab.Navigator>
+//       </NavigationContainer>
+//     </Provider>
+//   );
+// };
+
+const RootStack = createStackNavigator(
+  {
+    Login: Login,
+    Register: Register,
+    Home: Home,
+    Reset: Reset,
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#19AC52',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  },
+);
+
+const RootContainer = createAppContainer(RootStack);
+
+export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="User" component={UserScreen} />
-          <Tab.Screen name="API" component={ApiScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <RootContainer />
     </Provider>
   );
-};
-
-export default App;
+}
--- frontend/pantry/src/components/auth/Login.tsx
+++ frontend/pantry/src/components/auth/Login.tsx
@@ -1,16 +1,21 @@
-import React, { useState useEffect } from 'react';
-import { Button, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, ActivityIndicator, View, Text, Alert } from 'react-native';
+import { Button, Input, Icon } from 'react-native-elements';
+import auth from '@react-native-firebase/auth';
 
+export const Login = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showLoading, setShowLoading] = useState(false);
 
-export const Login = ({navigation}) => {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Login</Text>
-        </View>
-    )
-}
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Login</Text>
+    </View>
+  );
+};
 
 Login.navigationOptions = ({ navigation }) => ({
-    title: 'Login',
-    headerShown: false,
+  title: 'Login',
+  headerShown: false,
 });
