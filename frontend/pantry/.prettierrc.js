0d340fab55b5424571d1c5266269f5c6d826f3c7
Michael David Amparo <michaelamparo@pursuit.org>
2020-04-17 14:44:07 -0400

    changed tab with 4 -> 2 and modified run lint

--- frontend/pantry/.prettierrc.js
+++ frontend/pantry/.prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-    bracketSpacing: true,
-    jsxBracketSameLine: true,
-    singleQuote: true,
-    trailingComma: 'all',
-    tabWidth: 4,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
 };
