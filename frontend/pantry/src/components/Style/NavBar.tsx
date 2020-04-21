24c2fd97b6650b928c0dc1469bcbe57b38e8fd68
Michael David Amparo <michaelamparo@pursuit.org>
2020-04-20 20:26:32 -0400

    prettified navbar

--- frontend/pantry/src/components/Style/NavBar.tsx
+++ frontend/pantry/src/components/Style/NavBar.tsx
@@ -4,13 +4,6 @@ import FeatherIcon from 'react-native-vector-icons/Feather';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 
-/*
-    <FeatherIcon name="shopping-cart" style={styles.icon7}></FeatherIcon>
-    <FeatherIcon name="settings" style={styles.icon13}></FeatherIcon>
-    <FeatherIcon name="plus-circle" style={styles.icon16}></FeatherIcon>
-    <FeatherIcon name="shopping-bag" style={styles.icon14}></FeatherIcon>
-    <FeatherIcon name="list" style={styles.icon15}></FeatherIcon>
-*/
 import HomeScreen from '../Screens/Welcome.tsx';
 import UserScreen from '../Screens/UserScreen.tsx';
 import ApiScreen from '../Screens/ApiScreen.tsx';
@@ -25,44 +18,50 @@ const NavBar = (props: any) => {
         <Tab.Navigator
           screenOptions={({ route }) => ({
             tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-              let newSize = size;
-              switch (route.name) {
-                case 'Cart':
-                  iconName = 'shopping-cart';
-                  break;
-                case 'Settings':
-                  iconName = 'settings';
-                  break;
-                case 'Upload':
-                  iconName = 'plus-circle';
-                  newSize *= 1.5;
-                  break;
-                case 'User':
-                  iconName = 'shopping-bag';
-                  break;
-                case 'API':
-                  iconName = 'list';
-                  break;
-                default:
-                  break;
-              }
-
-              // You can return any component that you like here!
+              let iconConfig = {
+                Pantry: {
+                  name: 'shopping-bag',
+                  size: size * 1.25,
+                },
+                Cart: {
+                  name: 'shopping-cart',
+                  size: size * 1.25,
+                },
+                Upload: {
+                  name: 'plus-circle',
+                  size: size * 1.75,
+                },
+                List: {
+                  name: 'list',
+                  size: size * 1.25,
+                },
+                Settings: {
+                  name: 'settings',
+                  size: size * 1.25,
+                },
+              };
+              let icon = iconConfig[route.name];
               return (
-                <FeatherIcon name={iconName} size={newSize} color={color} />
+                <FeatherIcon name={icon.name} size={icon.size} color={color} />
               );
             },
           })}
           tabBarOptions={{
+            showLabel: false,
             activeTintColor: '#ff5c61',
             inactiveTintColor: 'gray',
+            style: {
+              paddingVertical: 5,
+              paddingHorizontal: 40,
+              justifyContent: 'space-around',
+              backgroundColor: '#fff',
+            },
           }}>
+          <Tab.Screen name="Pantry" component={UserScreen} />
           <Tab.Screen name="Cart" component={HomeScreen} />
-          <Tab.Screen name="Settings" component={HomeScreen} />
           <Tab.Screen name="Upload" component={ImageUpload} />
-          <Tab.Screen name="User" component={UserScreen} />
-          <Tab.Screen name="API" component={ApiScreen} />
+          <Tab.Screen name="List" component={ApiScreen} />
+          <Tab.Screen name="Settings" component={HomeScreen} />
         </Tab.Navigator>
       </NavigationContainer>
     </>
