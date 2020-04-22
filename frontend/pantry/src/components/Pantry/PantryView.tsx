8cc82cd4f264efd39bab8bd0ed9c1044985d51c3
Michael David Amparo <michaelamparo@pursuit.org>
2020-04-22 12:02:42 -0400

    renamed pantry to pantryview and added view with text

---
+++ frontend/pantry/src/components/Pantry/PantryView.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const PantryView = () => {
+  return (
+    <View>
+      <Text>This will be my pantry view containing all the food items</Text>
+    </View>
+  );
+};
