0681a2eb2faa5f5ff7ce86d334d3e0f47bca89e9
Michael David Amparo <michaelamparo@pursuit.org>
2020-04-22 19:11:51 -0400

    dummy data for food items

---
+++ frontend/pantry/src/components/Reciepts/dummyData/dummyFoodItems.ts
@@ -0,0 +1,62 @@
+export default [
+  {
+    productName: '-',
+    uri: '',
+    size: null,
+    quantity: null,
+    purchasedDate: null,
+  },
+  {
+    productName: 'Target',
+    uri: 'https://1000logos.net/wp-content/uploads/2017/06/Target-Logo.png',
+    size: 53.33,
+    quantity: 18,
+    purchasedDate: '03/22/2019',
+  },
+  {
+    productName: 'Walmart',
+    uri:
+      'https://pbs.twimg.com/profile_images/687029320926154752/hT10haXh_400x400.jpg',
+    size: 33.72,
+    quantity: 5,
+    purchasedDate: '03/03/2019',
+  },
+  {
+    productName: 'Whole Foods',
+    uri:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Whole_Foods_Market_201x_logo.svg/1200px-Whole_Foods_Market_201x_logo.svg.png',
+    size: 57.33,
+    quantity: 3,
+    purchasedDate: '01/07/2019',
+  },
+  {
+    productName: 'Walmart',
+    uri:
+      'https://pbs.twimg.com/profile_images/687029320926154752/hT10haXh_400x400.jpg',
+    size: 48.15,
+    quantity: 15,
+    purchasedDate: '03/11/2019',
+  },
+  {
+    productName: 'Target',
+    uri: 'https://1000logos.net/wp-content/uploads/2017/06/Target-Logo.png',
+    size: 11.49,
+    quantity: 2,
+    purchasedDate: '03/14/2019',
+  },
+  {
+    productName: 'Food Bazaar',
+    uri:
+      'https://www.foodbazaar.com/wp-content/themes/shoptocook-responsive-myfoodbazaar-2/images/facebook-logo.png',
+    size: 53.33,
+    quantity: 20,
+    purchasedDate: '03/28/2019',
+  },
+  {
+    productName: 'Target',
+    uri: 'https://1000logos.net/wp-content/uploads/2017/06/Target-Logo.png',
+    size: 40.87,
+    quantity: 12,
+    purchasedDate: '02/17/2019',
+  },
+];
