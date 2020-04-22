e73a3ef2622805030d6f1e0d1530182d56e8fb27
Michael David Amparo <michaelamparo@pursuit.org>
2020-04-22 13:04:09 -0400

    added food items to view and changed view to scroll view

--- frontend/pantry/src/components/Pantry/PantryView.tsx
+++ frontend/pantry/src/components/Pantry/PantryView.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+
+import FoodItem from './FoodItem.tsx';
 
 const PantryView = () => {
   return (
-    <View>
+    <ScrollView>
       <Text>This will be my pantry view containing all the food items</Text>
-    </View>
+      <FoodItem />
+      <FoodItem />
+      <FoodItem />
+      <FoodItem />
+    </ScrollView>
   );
 };
 
