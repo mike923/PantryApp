0937b7526123b8cb90f57572e59dd6cde66bf132
Michael David Amparo <michaelamparo@pursuit.org>
2020-04-22 22:17:21 -0400

    changes made right before presenting

--- frontend/pantry/src/components/Pantry/PantryView.tsx
+++ frontend/pantry/src/components/Pantry/PantryView.tsx
@@ -3,13 +3,16 @@ import { View, Text, StyleSheet } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
 import FoodItem from './FoodItem.tsx';
+import Foods from '../Reciepts/dummyData/dummyFoodItems.ts';
 
 const PantryView = () => {
   return (
     <ScrollView style={{ backgroundColor: 'white', flex: 1 }}>
       <View style={styles.pantryView}>
         <Text>This will be my pantry view containing all the food items</Text>
-        {Array(20).fill(<FoodItem />)}
+        {Foods.map((p) => (
+          <FoodItem {...p} />
+        ))}
       </View>
     </ScrollView>
   );
