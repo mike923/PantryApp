e451e0af0658f31d9c789dc012f67c8fa49f42e6
Michael David Amparo <michaelamparo@pursuit.org>
2020-04-26 14:27:32 -0400

    added cors

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors")
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -11,6 +12,7 @@ const authRouter = require("./routes/auth");
 
 const app = express();
 
+app.use(cors())
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
