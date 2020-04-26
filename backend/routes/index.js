ba21a1a1f6f426838534e4b5b44df5eb67a5afe6
Michael David Amparo <michaelamparo@pursuit.org>
2020-04-26 14:25:59 -0400

    added auth check to index

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+let authorized = false
+const checkAuth = (req, res, next) => {
+  if (authorized) {
+    next()
+  } else {
+    res.status(403).send('Unauthorized!')
+    return
+  }
+}
+
+
 /* GET home page. */
+router.use('/', checkAuth)
+
 router.get('/', function(req, res, next) {
-  res.json({ title: 'Express' });
+  res.json({ title: 'Welcome to Pantry Backend' });
 });
 
 module.exports = router;
