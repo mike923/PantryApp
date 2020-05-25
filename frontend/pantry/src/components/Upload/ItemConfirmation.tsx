7c0eb8892baa26d54537ff99bdd4af18c4bf5ec3
Michael David Amparo <michaelamparo@pursuit.org>
2020-05-24 20:05:56 -0400

    fixing regex

--- frontend/pantry/src/components/Upload/ItemConfirmation.tsx
+++ frontend/pantry/src/components/Upload/ItemConfirmation.tsx
@@ -32,7 +32,7 @@ const ItemConfirmation = (props: any) => {
       {
         text: 'Yes',
         onPress: async () => {
-          const data = await client.get('/test');
+          const data = await client.post('/receipts/upload');
           console.log(data);
           // TODO POST RECEIPT TO BACKEND
           navigation.dispatch(
