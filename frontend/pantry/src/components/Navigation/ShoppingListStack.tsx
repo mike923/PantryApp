acb0805ffdb7d31b1fb02ebc29ba89f8c1f14300
vonbarown <vonielbrown@pursuit.org>
2020-05-25 00:52:23 -0400

    fixing spelling error

--- frontend/pantry/src/components/Navigation/ShoppingListStack.tsx
+++ frontend/pantry/src/components/Navigation/ShoppingListStack.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { screenOptions } from './style.ts';
-import ShoppingList from '../ShoppinList/ShoppingList.tsx';
+import ShoppingList from '../ShoppingList/ShoppingList.tsx';
 
 const Stack = createStackNavigator();
 
