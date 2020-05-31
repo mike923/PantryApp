d3ba45afc856fd58265e8552d275184e21a4264b
vonbarown <vonielbrown@pursuit.org>
2020-05-31 13:48:06 -0400

    adding item upload functionality scannened items

--- frontend/pantry/src/components/ShoppingCart/ShopCart.tsx
+++ frontend/pantry/src/components/ShoppingCart/ShopCart.tsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import List from '../SwipeAbleList/SwipeAbleList.tsx';
 import EmptyCart from './emptyCart.tsx';
 import { deleteItem } from '../../redux/actions/cameraActions.ts';
+import { client } from '../../../proxy';
 
 const ShopCart = ({ navigation }: any) => {
   const products: any = useSelector((state: any) => state.camera.products);
@@ -20,27 +21,41 @@ const ShopCart = ({ navigation }: any) => {
 
   const constructProductObj = () => {
     products.forEach((el: any) => {
-      console.log('elele', el);
       let key = el.upc;
       if (productObj[key]) {
         productObj[key].quantity += 1;
-        // el['quantity'] = (el['quantity'] || 0) + 1;
       } else {
         el.quantity = 1;
         productObj[key] = el;
       }
     });
-
     setLocalProducts(Object.values(productObj));
     return Object.values(productObj);
   };
   console.log('set', productObj);
   console.log('products', localProducts);
 
-  // let newProductArr = Object.values(productObj);
+  const uploadScannedItem = async (itemObj: any) => {
+    try {
+      const { data } = await client.post(`/fooditem/add`, {
+        quantity: itemObj.quantity,
+        price: itemObj.price,
+        upc: itemObj.upc,
+        imgUrl: itemObj.image,
+        preferred_name: itemObj.name,
+      });
+      console.log('food item upload', data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return products.length ? (
-    <List data={localProducts} deleteItem={() => dispatch(deleteItem)} />
+    <List
+      data={localProducts}
+      deleteItem={() => dispatch(deleteItem)}
+      uploadScannedItem={uploadScannedItem}
+    />
   ) : (
     <EmptyCart navigation={navigation} />
   );
