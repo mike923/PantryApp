48b9571e0b2b11148a596f0b8497737ca37f5119
vonbarown <vonielbrown@pursuit.org>
2020-05-31 08:53:17 -0400

    fixing network connection functionality

--- frontend/pantry/src/redux/reducers/rootReducer.ts
+++ frontend/pantry/src/redux/reducers/rootReducer.ts
@@ -3,12 +3,14 @@ import { testReducer } from './testReducer.ts';
 import { userReducer } from './userReducer.ts';
 import { cameraReducer } from './cameraReducers.ts';
 import { textRecogReducer } from './textRecogReducers.ts';
+import { appReducer } from './appReducer.ts';
 
 const rootReducer = combineReducers({
   testUser: testReducer,
   user: userReducer,
   camera: cameraReducer,
   recog: textRecogReducer,
+  appReducer,
 });
 
 export default rootReducer;
